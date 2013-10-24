90ab3a3ef8c579f4fdaf8e9aded85c04c66f4d6d
See <rsee@wm720l21019a.(none)>
2013-10-24 14:27:58 -0700

    m

--- API/index.js
+++ API/index.js
@@ -38,6 +38,18 @@ app.get('/movies', function(req, res) {
   });
 });
 
+app.get('/movies/sortByVote', function(req, res) {
+  Movie.sortByVote(function(err, dudes) {
+    if(err) {
+      res.send({
+        error: err.message
+      });
+    }
+
+    res.send(dudes);
+  });
+});
+
 app.get('/movie/:id', function(req, res) {
   Movie.get(req.params.id, function(err, dude) {
     if(err) {
--- API/models/movie.js
+++ API/models/movie.js
@@ -39,6 +39,12 @@ var Movie = {
     //param, it will return all users    
     this.model.find({}, {__v: 0, _id: 0}, callback);
   },
+          
+  sortByVote: function(callback) {
+    //find will return an array of users. Without a query for the first
+    //param, it will return all users    
+    this.model.find().sort({'votes': 1}).exec(callback);
+  },
 
   delete: function(id, callback) {
     this.model.remove({id: id}, callback);
