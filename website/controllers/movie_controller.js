a1662c3c3be6b112362b04790bef514c4653aaac
shailbenq <benq019@gmail.com>
2013-10-24 13:43:21 -0700

    movie controller update

---
+++ website/controllers/movie_controller.js
@@ -0,0 +1,57 @@
+var request = require('request');
+
+
+var Movie = {
+  get: function(id, callback) {
+    var url = '/movie/' + id; 
+
+    if(typeof id === 'function') {
+      callback = id;
+      url = '/movies';
+    }
+
+    request({
+      url: process.config.api + url, 
+      json: true
+    }, function (err, response, body) {
+      if(err) {
+        return callback({
+          error: err.message
+        });
+      }
+
+      if (response.statusCode !== 200) {
+        return callback({
+          error: 'Response returned code ' + response.statusCode
+        });
+      }
+
+      callback(null, body);
+    });
+  },
+
+  create: function(data, callback) {
+    request({
+      url: process.config.api + '/movie',
+      method: 'POST', 
+      json: true,
+      body: data
+    }, function (err, response, body) {
+      if(err) {
+        return callback({
+          error: err.message
+        });
+      }
+
+      if (response.statusCode !== 200) {
+        return callback({
+          error: 'Response returned code ' + response.statusCode
+        });
+      }
+
+      callback(null, body);
+    });
+  }
+};
+
+module.exports = Movie;
